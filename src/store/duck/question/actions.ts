b3703ba12c42ef6bb619e5bddd05acca327fec4d
kgordeew <Kirill.Gordeew@firstlinesoftware.com>
2021-04-12 11:26:21 +0300

    FR-1 added constants, actions for liked and disliked

--- src/store/duck/question/actions.ts
+++ src/store/duck/question/actions.ts
@@ -1,9 +1,11 @@
-import { createAsyncAction } from 'typesafe-actions';
+import { createAsyncAction, createAction } from 'typesafe-actions';
 
 import {
   FETCH_QUESTION_REQUEST,
   FETCH_QUESTION_SUCCESS,
   FETCH_QUESTION_FAILURE,
+  SET_LIKED,
+  SET_DISLIKED,
 } from './constants';
 
 import { IQuestion } from './interfaces';
@@ -12,10 +14,15 @@ const fetchQuestion = createAsyncAction(
   FETCH_QUESTION_REQUEST,
   FETCH_QUESTION_SUCCESS,
   FETCH_QUESTION_FAILURE,
-)<boolean, IQuestion, never>();
+)<string, IQuestion, Error>();
+
+const setLiked = createAction(SET_LIKED)<IQuestion['id'][]>();
+const setDisliked = createAction(SET_DISLIKED)<IQuestion['id'][]>();
 
 export const questionActions = {
   fetchQuestionRequest: fetchQuestion.request,
   fetchQuestionSuccess: fetchQuestion.success,
   fetchQuestionFailure: fetchQuestion.failure,
+  setLiked,
+  setDisliked,
 };
