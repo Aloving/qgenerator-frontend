0e03ebd2ef0e23955cc4b4539210883635b8484e
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-05-05 20:39:32 +0300

    FR-15 Clean up env.ts

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,5 +3,4 @@ import { get } from '../config';
 console.log(get('LANGUAGES'));
 
 export const DEFAULT_LANGUAGE = get('DEFAULT_LANGUAGE');
-// export const LANGUAGES = get('LANGUAGES').split(', ');
 export const LANGUAGES = JSON.parse(get('LANGUAGES')) as string[];
