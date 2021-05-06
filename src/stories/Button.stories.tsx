2b6edd59ad44d86b0e73bab7b37fb7bfb56dbbc8
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-05-06 11:39:42 +0300

    FR-5 Update Button stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,12 +1,9 @@
-// Button.stories.tsx
-
 import React from 'react';
 
 import { Story } from '@storybook/react';
 
 import { ButtonProps, Button } from '../modules/common/components/Button';
 
-//👇 We create a “template” of how args map to rendering
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
@@ -14,6 +11,21 @@ export const Default = Template.bind({});
 Default.args = {
   children: 'Click me!',
   color: 'primary',
+};
+
+export const Outlined = Template.bind({});
+
+Outlined.args = {
+  children: 'Click me!',
+  color: 'primary',
+  variant: 'outlined',
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  children: 'Click me!',
+  color: 'secondary',
 };
 
 export default {
