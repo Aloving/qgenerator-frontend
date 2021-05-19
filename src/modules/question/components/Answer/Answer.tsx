dec104035e801d5f784608e9a5f2cd09a1e08e67
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-05-20 00:16:48 +0300

    FR-8 Add loading state for Answer

--- src/modules/question/components/Answer/Answer.tsx
+++ src/modules/question/components/Answer/Answer.tsx
@@ -5,6 +5,7 @@ import cn from 'classnames';
 import { Likes } from '../../../common/components';
 
 import styles from './Answer.module.css';
+import { Skeleton } from '@material-ui/lab';
 
 interface IAnswerProps {
   likes: number;
@@ -40,33 +41,52 @@ export const Answer: React.FC<IAnswerProps> = ({
   return (
     <div className={styles.root}>
       <div className={styles.avatarWrapper}>
-        <Avatar variant="square" src={avatar} className={styles.avatar} />
+        {isLoading ? (
+          <Skeleton variant="rect" width={48} height={48} />
+        ) : (
+          <Avatar variant="square" src={avatar} className={styles.avatar} />
+        )}
       </div>
       <div className="content">
         <div className={styles.header}>
           <div className="name">
-            <Typography variant="subtitle1" color="textPrimary">
-              {authorName}
-            </Typography>
+            {isLoading ? (
+              <Skeleton width={185} height={20} />
+            ) : (
+              <Typography variant="subtitle1" color="textPrimary">
+                {authorName}
+              </Typography>
+            )}
           </div>
           <div className={styles.timeWrapper}>
-            <Typography
-              variant="subtitle2"
-              color="secondary"
-              className={cn(classNames.time, styles.time)}
-            >
-              {time}
-            </Typography>
+            {isLoading ? (
+              <Skeleton width={95} height={20} />
+            ) : (
+              <Typography
+                variant="subtitle2"
+                color="secondary"
+                className={cn(classNames.time, styles.time)}
+              >
+                {time}
+              </Typography>
+            )}
           </div>
         </div>
         <div className={styles.textWrapper}>
-          <Typography
-            variant="caption"
-            color="textPrimary"
-            className={styles.text}
-          >
-            {text}
-          </Typography>
+          {isLoading ? (
+            <>
+              <Skeleton width={593} height={20} />
+              <Skeleton width={647} height={20} />
+            </>
+          ) : (
+            <Typography
+              variant="caption"
+              color="textPrimary"
+              className={styles.text}
+            >
+              {text}
+            </Typography>
+          )}
         </div>
         <div className="actions">
           <Likes
